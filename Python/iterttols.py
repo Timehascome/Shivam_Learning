77fd0f87c5b763bd290851d1648e5427a09418a7
Shivam Shukla <econnect.shivam@gmail.com>
2026-01-05 17:23:45 +0530

    Jan 05 changes

---
+++ Python/iterttols.py
@@ -0,0 +1,10 @@
+        # we learn itertools module
+import itertools    
+# Example: Using itertools to create a Cartesian product of two lists
+list1 = [1, 2]  
+list2 = ['a', 'b']  
+cartesian_product = list(itertools.product(list1, list2))   
+print("Cartesian Product:", cartesian_product)
+#we do tuple packing and unpacking using cartesian product
+a, b = cartesian_product[0]  # Unpacking the first tuple    
+print("Unpacked values:", a, b)
